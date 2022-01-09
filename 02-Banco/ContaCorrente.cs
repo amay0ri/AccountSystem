3b8fea523198bdacd5622feb1ee629e037797afd
mathe <mathe@MATHEUS>
2022-01-09 17:25:27 -0300

    Adicionar arquivos de projeto.

---
+++ 02-Banco/ContaCorrente.cs
@@ -0,0 +1,90 @@
+﻿
+
+
+namespace Banco
+{
+    public class ContaCorrente
+    {
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotaldeContas++;
+        }
+        public static int TotaldeContas { get; private set; }
+
+       
+        public Cliente Titular { get; set; }
+        private int _agencia;
+        public int Agencia 
+        { 
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    return;
+                }
+               _agencia = value;
+            } }   
+        public int Numero { get; set; }
+        private double _saldo;
+
+
+
+
+
+
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set { 
+                
+                if (value < 0)
+                {
+                    return;
+
+                }
+
+                _saldo = value;
+            }
+        } 
+
+
+
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente conta)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            conta.Depositar(valor);
+            return true;
+        }
+    }
+
+
+}
---
+++ 02-Banco/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           ContaCorrente c1 = new ContaCorrente(77,8250);
+            Console.WriteLine(ContaCorrente.TotaldeContas);
+            ContaCorrente c2 = new ContaCorrente(40, 300);
+            Console.WriteLine(ContaCorrente.TotaldeContas);
+
+            c1.Saldo = 100.0;
+            c1.Titular = new Cliente();
+            c1.Titular.Nome = "Rogério";
+
+            Console.WriteLine("O saldo de " + c1.Titular.Nome + " é de: " + c1.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
